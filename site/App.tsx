9b3e259ee294428ee1ec2b15b907cb263b6d3e15
Kyle Durand <kyle.durand@shopify.com>
2021-05-27 23:11:30 -0400

    Add overlay component

--- site/App.tsx
+++ site/App.tsx
@@ -7,35 +7,33 @@ function App() {
   const closeModal = useCallback(() => setOpen(false), [setOpen]);
 
   return (
-    <Portal style={{width: '100%', height: '100%'}}>
-      <Modal open={open} onDismiss={() => {}}>
-        <Button type="button" onClick={() => setOpen(true)} variant="primary">
-          Open modal
-        </Button>
-        <Modal.Dialog ariaLabel="TODO: change this prop API">
-          <Card>
-            <Card.Section>
-              <Text size="heading">Title</Text>
-            </Card.Section>
-            <Card.Section>
-              <Text size="body">body</Text>
-            </Card.Section>
-            <Card.Section>
-              <Flex align="end" gap="3">
-                <Button onClick={closeModal}>Cancel</Button>
-                <Button variant="primary" onClick={closeModal}>
-                  Save
-                </Button>
-              </Flex>
-            </Card.Section>
-          </Card>
-        </Modal.Dialog>
-        <Modal.Overlay
-          css={{backgroundColor: 'rgb(76 175 80 / 0.5)'}}
-          onClick={() => setOpen(false)}
-        />
-      </Modal>
-    </Portal>
+    <Modal open={open} onDismiss={() => {}}>
+      <Button type="button" onClick={() => setOpen(true)} variant="primary">
+        Open modal
+      </Button>
+      <Modal.Dialog ariaLabel="TODO: change this prop API">
+        <Card>
+          <Card.Section>
+            <Text size="heading">Title</Text>
+          </Card.Section>
+          <Card.Section>
+            <Text size="body">body</Text>
+          </Card.Section>
+          <Card.Section>
+            <Flex align="end" gap="3">
+              <Button onClick={closeModal}>Cancel</Button>
+              <Button variant="primary" onClick={closeModal}>
+                Save
+              </Button>
+            </Flex>
+          </Card.Section>
+        </Card>
+      </Modal.Dialog>
+      <Modal.Overlay
+        css={{backgroundColor: 'rgb(76 175 80 / 0.5)'}}
+        onClick={() => setOpen(false)}
+      />
+    </Modal>
   );
 }
 
