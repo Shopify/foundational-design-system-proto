f06184f9fb82ff018a77aabd6f2d75a7fb448ca2
Kyle Durand <kyledurand@users.noreply.github.com>
2021-05-31 11:51:58 -0400

    Add overlay component (#12)
    * Add overlay component
    
    * Add overlay component
    
    * remove portal import

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -3,5 +3,8 @@ export * from './Card';
 export * from './Flex';
 export * from './Grid';
 export * from './Icon';
+export * from './Overlay';
 export * from './Portal';
 export * from './Text';
+
+export type {Props as OverlayProps} from './Overlay';
--- site/App.tsx
+++ site/App.tsx
@@ -7,31 +7,33 @@ function App() {
   const closeModal = useCallback(() => setOpen(false), [setOpen]);
 
   return (
-    <>
-      <Modal open={open} onDismiss={() => {}}>
-        <Button type="button" onClick={() => setOpen(true)} variant="primary">
-          Open modal
-        </Button>
-        <Modal.Dialog ariaLabel="TODO: change this prop API">
-          <Card>
-            <Card.Section>
-              <Text size="heading">Title</Text>
-            </Card.Section>
-            <Card.Section>
-              <Text size="body">body</Text>
-            </Card.Section>
-            <Card.Section>
-              <Flex align="end" gap="3">
-                <Button onClick={closeModal}>Cancel</Button>
-                <Button variant="primary" onClick={closeModal}>
-                  Save
-                </Button>
-              </Flex>
-            </Card.Section>
-          </Card>
-        </Modal.Dialog>
-      </Modal>
-    </>
+    <Modal open={open} onDismiss={() => {}}>
+      <Button type="button" onClick={() => setOpen(true)} variant="primary">
+        Open modal
+      </Button>
+      <Modal.Dialog ariaLabel="TODO: change this prop API">
+        <Card>
+          <Card.Section>
+            <Text size="heading">Title</Text>
+          </Card.Section>
+          <Card.Section>
+            <Text size="body">body</Text>
+          </Card.Section>
+          <Card.Section>
+            <Flex align="end" gap="3">
+              <Button onClick={closeModal}>Cancel</Button>
+              <Button variant="primary" onClick={closeModal}>
+                Save
+              </Button>
+            </Flex>
+          </Card.Section>
+        </Card>
+      </Modal.Dialog>
+      <Modal.Overlay
+        css={{backgroundColor: 'rgb(76 175 80 / 0.5)'}}
+        onClick={() => setOpen(false)}
+      />
+    </Modal>
   );
 }
 
