e90f16c8d891ea657ed283dbe7814f4a427e5341
Sam Rose <sam.rose@shopify.com>
2021-06-23 13:35:55 -0400

    Refactor Text to use createElement instead of Box

--- src/packages/elements/src/components/Text/Text.tsx
+++ src/packages/elements/src/components/Text/Text.tsx
@@ -1,7 +1,12 @@
-import React, {ReactNode, ElementType} from 'react';
+import {
+  createElement,
+  forwardRef,
+  AllHTMLAttributes,
+  ElementType,
+  ReactNode,
+} from 'react';
 import classnames from 'classnames';
 
-import {Box} from '../Box';
 import {atoms, Atoms} from '../../atoms/atoms.css';
 
 import * as styles from './Text.css';
@@ -12,7 +17,9 @@ interface TextStyleProps {
   type?: Exclude<keyof typeof styles.font, 'brand' | 'heading'>;
 }
 
-export interface TextProps extends TextStyleProps {
+export interface TextProps
+  extends TextStyleProps,
+    Omit<AllHTMLAttributes<HTMLElement>, 'type'> {
   component?: ElementType;
   children: ReactNode;
 }
@@ -28,16 +35,15 @@ export const useTextStyles = ({
     styles.weight[weight],
   );
 
-export const Text = ({
-  component = 'span',
-  weight,
-  align,
-  type,
-  children,
-}: TextProps) => {
-  return (
-    <Box component={component} className={useTextStyles({weight, type, align})}>
-      {children}
-    </Box>
-  );
-};
+export const Text = forwardRef<HTMLElement, TextProps>(
+  ({component = 'span', className, weight, align, type, children}, ref) => {
+    return createElement(
+      component,
+      {
+        className: classnames(useTextStyles({weight, type, align}), className),
+        ref,
+      },
+      children,
+    );
+  },
+);
