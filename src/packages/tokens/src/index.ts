0aea1fc64bc919f259f038420096598f66b5205d
Sam Rose <sam.rose@shopify.com>
2021-06-23 13:35:30 -0400

    Remove unused tokens (grid and breakpoints)

--- src/packages/tokens/src/index.ts
+++ src/packages/tokens/src/index.ts
@@ -1,14 +1,12 @@
 import {colors} from './colors';
 import {fonts, fontSizes, fontWeights} from './typography';
-import {breakpoints, grid, spacing} from './spacing';
+import {spacing} from './spacing';
 
 export const tokens = {
-  breakpoints,
   colors,
   fonts,
   fontSizes,
   fontWeights,
-  grid,
   spacing,
 };
 
--- src/packages/tokens/src/spacing.ts
+++ src/packages/tokens/src/spacing.ts
@@ -3,16 +3,6 @@
 const base = 4;
 const px = (value: number) => `${value}px`;
 
-export const breakpoints = {
-  sm: '640px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1280px',
-  '2xl': '1536px',
-};
-
-export const grid = base;
-
 export const spacing = {
   1: px(base * 1),
   2: px(base * 1.5),
