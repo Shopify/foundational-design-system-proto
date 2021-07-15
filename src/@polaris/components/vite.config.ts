b778472b55c4c1af7a11814feaff1c85ecbb6de1
Sam Rose <sam.rose@shopify.com>
2021-07-15 06:44:49 -0400

    Initial build and publish configuration (#151)
    * Initial build and publish configuration
    
    * Move back to root dir after build
    
    Co-authored-by: Alex Page <alex@alexpage.com.au>
    
    Co-authored-by: Alex Page <alex@alexpage.com.au>

---
+++ src/@polaris/components/vite.config.ts
@@ -0,0 +1,32 @@
+import {resolve} from 'path';
+
+import {defineConfig} from 'vite';
+import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin';
+
+function getPackage(packageName: string, path = 'src') {
+  return resolve(__dirname, '../', packageName, path);
+}
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@polaris/layout-experimental',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@polaris/icons': getPackage('icons'),
+      '@polaris/tokens': getPackage('tokens'),
+    },
+  },
+});
