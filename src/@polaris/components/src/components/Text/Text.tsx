4c6e140b2585778686ddc1f44a7f80fdca6c214f
aveline <aveline@users.noreply.github.com>
2021-07-15 15:07:05 -0700

    Add color to Text (#171)

--- src/@polaris/components/src/components/Text/Text.tsx
+++ src/@polaris/components/src/components/Text/Text.tsx
@@ -16,11 +16,12 @@ interface TextStyleProps {
   fontSize?: Atoms['fontSize'];
   type?: Exclude<keyof typeof styles.font, 'brand' | 'heading'>;
   weight?: Atoms['fontWeight'];
+  color?: Atoms['color'];
 }
 
 export interface TextProps
   extends TextStyleProps,
-    Omit<AllHTMLAttributes<HTMLElement>, 'type'> {
+    Omit<AllHTMLAttributes<HTMLElement>, 'type' | 'color'> {
   component?: ElementType;
   children: ReactNode;
 }
@@ -30,22 +31,32 @@ export const useTextStyles = ({
   type = 'system',
   align,
   fontSize = 'base',
+  color,
 }: TextStyleProps) =>
   classnames(
     styles.font[type],
-    atoms({textAlign: align, fontSize, fontWeight: weight}),
+    atoms({textAlign: align, fontSize, fontWeight: weight, color}),
   );
 
 export const Text = forwardRef<HTMLElement, TextProps>(
   (
-    {component = 'span', className, weight, align, type, fontSize, children},
+    {
+      component = 'span',
+      className,
+      weight,
+      align,
+      type,
+      fontSize,
+      color,
+      children,
+    },
     ref,
   ) => {
     return createElement(
       component,
       {
         className: classnames(
-          useTextStyles({weight, type, align, fontSize}),
+          useTextStyles({weight, type, align, fontSize, color}),
           className,
         ),
         ref,
