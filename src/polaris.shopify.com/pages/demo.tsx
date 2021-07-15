b017173fc436b2723f5e97a045aef0b4535b3e84
Aveline Thelen <aveline.thelen@shopify.com>
2021-07-15 13:35:28 -0700

    Fix background colors

--- src/polaris.shopify.com/pages/demo.tsx
+++ src/polaris.shopify.com/pages/demo.tsx
@@ -33,23 +33,45 @@ const DemoPage = () => {
           overflow: 'hidden',
         }}
       >
-        <Box>
+        <Box color="gray-0" style={{position: 'relative'}}>
           <img src="/static/demo/hero.jpg" alt="Snowboarder" />
-          <Flex justifyContent="space-between">
+          <Flex
+            justifyContent="space-between"
+            style={{position: 'absolute', top: '50%', left: 0, right: 0}}
+          >
             <Icon source={ArrowLeftMinor} color="subdued" />
             <Icon source={MobileHorizontalDotsMajor} color="subdued" />
           </Flex>
 
-          <Box>
-            <img src="/static/demo/snowdevil.png" alt="SnowDevil" />
+          <Box
+            style={{
+              position: 'absolute',
+              width: '88px',
+              bottom: '-44px',
+              left: '10px',
+            }}
+          >
+            <img
+              src="/static/demo/snowdevil.png"
+              alt="SnowDevil"
+              width="100%"
+            />
           </Box>
         </Box>
-        <Box margin="5">
-          <Flex justifyContent="space-between">
+        <Box padding="5" backgroundColor="gray-0" paddingTop="24">
+          <Flex justifyContent="space-between" alignItems="center">
             <Text weight="strong" fontSize="5xl">
               SnowDevil
             </Text>
-            <Box>Follow</Box>
+            <Box
+              borderRadius="2"
+              paddingX="4"
+              paddingY="1"
+              backgroundColor="gray-900"
+              color="gray-0"
+            >
+              <Text fontSize="sm">Follow</Text>
+            </Box>
           </Flex>
           <Box as="p">
             Snowdevil is not your typical snowboard store. Instead, Snowdevil is
@@ -79,16 +101,21 @@ const DemoPage = () => {
 
           <Grid gap="4" columns={['1fr', '1fr']}>
             <Box>
-              <img
-                src="/static/demo/burton-throwback.jpg"
-                alt="Burton Throwback"
-              />
+              <Box borderRadius="4" style={{overflow: 'hidden'}}>
+                <img
+                  src="/static/demo/burton-throwback.jpg"
+                  alt="Burton Throwback"
+                />
+              </Box>
               <Text weight="medium">Burton Throwback</Text>
               <br />
               <Text> $175.00</Text>
             </Box>
             <Box>
-              <img src="/static/demo/burton-stylus.jpg" alt="Burton Stylus" />
+              <Box borderRadius="4" style={{overflow: 'hidden'}}>
+                <img src="/static/demo/burton-stylus.jpg" alt="Burton Stylus" />
+              </Box>
+
               <Text weight="medium">Burton Stylus Flat Top</Text>
               <br />
               <Text>$899.99</Text>
