61806a7750c09c4ea4bb1bf979feebebed0f2570
Sam Rose <sam.rose@shopify.com>
2021-08-10 12:48:32 -0400

    Replace jest with vite-jest
    Closes #207

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
+  preset: 'vite-jest',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  transform: {
-    '\\.tsx?$': ['babel-jest', {configFile: './babel-jest.config.js'}],
-  },
 };
