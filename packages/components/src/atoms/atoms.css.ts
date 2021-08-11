f654d3734d48001c3fc438775b913870f26b5322
Laura Griffee <laura.griffee@shopify.com>
2021-08-11 09:09:43 -0600

    Add additional cursor properties to atoms for drag&drop

--- packages/components/src/atoms/atoms.css.ts
+++ packages/components/src/atoms/atoms.css.ts
@@ -121,7 +121,7 @@ const styles = createAtomicStyles({
     borderWidth,
     boxShadow,
     color: colors,
-    cursor: ['default', 'pointer'],
+    cursor: ['default', 'pointer', 'grab', 'grabbing'],
     fontSize: fontSizes,
     fontWeight: fontWeights,
     opacity: [0.5, 0.8],
