56fa6adfc739fcf9c5d762e86502cedd2fde037c
Aveline Thelen <aveline.thelen@shopify.com>
2021-08-12 14:07:57 -0700

    Remove default exports

--- packages/tokens/src/shadows.ts
+++ packages/tokens/src/shadows.ts
@@ -3,5 +3,3 @@ export const shadows = {
   medium: '0 5px 23px rgba(0,0,0,0.12), 0 6px 7px rgba(0,0,0,0.20)',
   large: '0 9px 46px rgba(0,0,0,0.12), 0 11px 15px rgba(0,0,0,0.20)',
 };
-
-export default shadows;
