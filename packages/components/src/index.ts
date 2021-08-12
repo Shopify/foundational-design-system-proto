13fd04266a450dbc93ca75ecbaa7029e44fb5790
Sam Rose <sam.rose@shopify.com>
2021-08-12 13:47:12 -0400

    fix: Clean up package exports

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,10 +1,9 @@
-// Components
 export * from './Box';
-// export * from './Center';
-// export * from './Container';
-// export * from './Flex';
-// export * from './Grid';
-// export * from './Inline';
-// export * from './Link';
-// export * from './Stack';
-// export * from './Text';
+export * from './Center';
+export * from './Container';
+export * from './Flex';
+export * from './Grid';
+export * from './Inline';
+export * from './Link';
+export * from './Stack';
+export * from './Text';
