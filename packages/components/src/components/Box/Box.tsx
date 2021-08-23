a5d06ff828f1f6e9ea665848da442b262bcd2e31
Sam Rose <sam.rose@shopify.com>
2021-08-23 08:14:43 -0400

    Remove internal classNames parsing from Box

--- packages/components/src/components/Box/Box.tsx
+++ packages/components/src/components/Box/Box.tsx
@@ -1,38 +1,39 @@
 import React, {forwardRef, AllHTMLAttributes, ElementType} from 'react';
 
-import classNames, {ClassValue} from '../../utilities/classNames';
 import {atoms, Atoms} from '../../atoms';
+
+function isAtomsProp(key: string): key is keyof Atoms {
+  return atoms.properties.has(key as keyof Omit<Atoms, 'reset'>);
+}
 
 export interface BoxProps
   extends Omit<
       AllHTMLAttributes<HTMLElement>,
-      'height' | 'width' | 'color' | 'cursor' | 'className'
+      'height' | 'width' | 'color' | 'cursor'
     >,
     Atoms {
   component?: ElementType;
-  className?: ClassValue;
 }
 
 export const Box = forwardRef<HTMLElement, BoxProps>(
-  ({component: Component = 'div', className, ...props}: BoxProps, ref) => {
+  ({component: Component = 'div', className = '', ...props}: BoxProps, ref) => {
     const atomProps: {[key: string]: unknown} = {};
     const nativeProps: {[key: string]: unknown} = {};
 
     for (const key in props) {
-      if (atoms.properties.has(key as keyof Omit<Atoms, 'reset'>)) {
+      if (isAtomsProp(key)) {
         atomProps[key] = props[key as keyof typeof props];
       } else {
         nativeProps[key] = props[key as keyof typeof props];
       }
     }
 
-    const userClasses = classNames(className);
     const atomicClasses = atoms(atomProps);
 
     return (
       <Component
         ref={ref}
-        className={`${atomicClasses}${userClasses ? ` ${userClasses}` : ''}`}
+        className={`${atomicClasses} ${className}`}
         {...nativeProps}
       />
     );
