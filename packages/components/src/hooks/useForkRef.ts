38bea120f4b0c23f9975222f2c15d8bd2e9855a3
Aaron Casanova <aaron.casanova@shopify.com>
2021-08-25 14:17:48 -0700

    docs: Add links to derived sources

--- packages/components/src/hooks/useForkRef.ts
+++ packages/components/src/hooks/useForkRef.ts
@@ -11,6 +11,7 @@ import type {AnyRef} from '../utilities';
  * with the ref. Cleanup naturally emerges from this behavior
  *
  * Derived from: Material-UI and Rooks
+ * https://github.com/mui-org/material-ui/blob/c7a37397e78abb754280552429b43e07af156122/packages/material-ui-utils/src/useForkRef.ts#L4
  */
 export function useForkRef<T>(refA: AnyRef<T>, refB: AnyRef<T>): AnyRef<T> {
   return React.useMemo(() => {
