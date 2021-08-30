6926407e8bbbcf9b4bb769cda2c11b1acca1ceaf
Aaron Casanova <aaron.casanova@shopify.com>
2021-08-30 10:58:02 -0700

    feat: Simplify Portal implementation

--- packages/components/src/components/Portal/Portal.tsx
+++ packages/components/src/components/Portal/Portal.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import clsx from 'clsx';
 import type * as Polymorphic from '@radix-ui/react-polymorphic';
 
-import {useMergeRefs, useIsomorphicLayoutEffect} from '../../hooks';
-import {isValidElementWithRef, assignRef} from '../../utilities';
 import {useTheme} from '../../theme';
 
 interface Props {
@@ -17,12 +15,6 @@ interface Props {
    * `document.body` is applied by default.
    */
   container?: Element;
-
-  /**
-   * Disables the portal allowing the children to stay within their
-   * current DOM hierarchy.
-   */
-  disablePortal?: boolean;
 }
 
 type PolymorphicPortal = Polymorphic.ForwardRefComponent<'div', Props>;
@@ -32,69 +24,19 @@ export type PortalProps = Polymorphic.OwnProps<PolymorphicPortal>;
 /**
  * Portals provide a first-class way to render children into a DOM node
  * that exists outside the DOM hierarchy of the parent component.
- *
- * Derived from Material-UI's [Portal](https://github.com/mui-org/material-ui/blob/120c564109245a3b070b1cbaf2c9f3f8659fd9fa/packages/material-ui/src/Portal/Portal.js#L21)
  */
 export const Portal = React.forwardRef(function Portal(props, ref) {
-  const {
-    as: Component = 'div',
-    children,
-    className,
-    container: containerProp,
-    disablePortal = false,
-    ...restProps
-  } = props;
+  const {as: Component = 'div', className, container, ...restProps} = props;
 
-  const [container, setContainer] = React.useState<null | Element>(null);
   const {themeClass} = useTheme();
 
-  useIsomorphicLayoutEffect(() => {
-    if (!disablePortal) {
-      setContainer(containerProp || document.body);
-    }
-  }, [containerProp, disablePortal]);
-
-  useIsomorphicLayoutEffect(() => {
-    if (container && !disablePortal) {
-      assignRef(ref, container);
-
-      return () => {
-        assignRef(ref, null);
-      };
-    }
-
-    return undefined;
-  }, [ref, container, disablePortal]);
-
-  const mergedRefs = useMergeRefs(
-    React.isValidElement(children)
-      ? (children as {ref?: React.Ref<unknown>})?.ref
-      : null,
-    ref,
+  return ReactDOM.createPortal(
+    <Component
+      ref={ref}
+      className={clsx(themeClass, className)}
+      {...restProps}
+    />,
+    container ?? document.body,
+    props.key,
   );
-
-  const rootProps = {
-    ...restProps,
-    className: clsx(themeClass, className),
-  };
-
-  if (disablePortal) {
-    let content = children;
-
-    if (React.isValidElement(children)) {
-      content = React.cloneElement(children, {
-        ref: mergedRefs,
-      });
-    }
-
-    return <Component {...rootProps}>{content}</Component>;
-  }
-
-  return container
-    ? ReactDOM.createPortal(
-        <Component {...rootProps}>{children}</Component>,
-        container,
-        props.key,
-      )
-    : container;
 }) as PolymorphicPortal;
