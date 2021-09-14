91e9407aab07a5ae7f3ee37b02d6e28d72ac241b
Aveline Thelen <aveline.thelen@shopify.com>
2021-09-14 15:14:33 -0700

    Update index.ts

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -1,5 +1,7 @@
 // Components
 export * from './Box';
+export * from './Button';
+export * from './ButtonBase';
 export * from './Center';
 export * from './Container';
 export * from './Flex';
@@ -9,5 +11,3 @@ export * from './Inline';
 export * from './Link';
 export * from './Portal';
 export * from './Stack';
-export * from './ButtonBase';
-export * from './Button';
